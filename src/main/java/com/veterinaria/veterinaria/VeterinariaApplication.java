830874e7a15706b01d9278737ffac4c328f0bc22
AlejoGalloM <galloalejo2@gmail.com>
2020-03-03 18:28:28 -0500

    Primer commit :'V

---
+++ src/main/java/com/veterinaria/veterinaria/VeterinariaApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinariaApplication.class, args);
+    }
+
+}
