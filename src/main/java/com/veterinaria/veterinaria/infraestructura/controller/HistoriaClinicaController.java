6911230a8e1a8c3f185746aa90eb4da9363e9372
AlejoGalloM <galloalejo2@gmail.com>
2020-11-26 12:11:19 -0500

    CrossOrigin

--- src/main/java/com/veterinaria/veterinaria/infraestructura/controller/HistoriaClinicaController.java
+++ src/main/java/com/veterinaria/veterinaria/infraestructura/controller/HistoriaClinicaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/historia-clinica")
 public class HistoriaClinicaController {
--- src/main/java/com/veterinaria/veterinaria/infraestructura/controller/PacienteController.java
+++ src/main/java/com/veterinaria/veterinaria/infraestructura/controller/PacienteController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/paciente")
 public class PacienteController {
